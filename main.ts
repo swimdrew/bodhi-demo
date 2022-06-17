464ddd048bcc1e39f0bbaa5f3746d79ac1897196
swimdrew <andrew@kvasium.com>
2022-06-16 22:36:18 -0400

    Format the file

--- main.ts
+++ main.ts
@@ -2,20 +2,22 @@ import { Provider } from "@acala-network/bodhi";
 import { createApiOptions } from "@acala-network/eth-providers";
 import { WsProvider } from "@polkadot/api";
 
-
 async function main() {
   const evmprovider = new Provider(
     createApiOptions({
       provider: new WsProvider("wss://karura-rpc-0.aca-api.network"),
-    }),
+    })
   );
 
-
-// Grab the following tx:
-// https://blockscout.karura.network/tx/0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e/token-transfers
-const tx = await evmprovider.getTXReceiptByHash("0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e");
-const recepit = await evmprovider.getTransactionByHash("0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e");
-console.log(`tx: ${tx}\nreceipt: ${recepit}`);
+  // Grab the following tx:
+  // https://blockscout.karura.network/tx/0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e/token-transfers
+  const tx = await evmprovider.getTXReceiptByHash(
+    "0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e"
+  );
+  const recepit = await evmprovider.getTransactionByHash(
+    "0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e"
+  );
+  console.log(`tx: ${tx}\nreceipt: ${recepit}`);
 }
 
 main();
