2b2ec592e542175ddda0e9a72047257a5935ea76
swimdrew <andrew@kvasium.com>
2022-06-16 22:42:44 -0400

    Get balance too for sanity

--- main.ts
+++ main.ts
@@ -8,7 +8,9 @@ async function main() {
       provider: new WsProvider("wss://karura-rpc-0.aca-api.network"),
     })
   );
-
+  const balance = await evmprovider.getBalance(
+    "0x1c3D657F0518A094BF351852bad4285EFc0D5Ce9"
+  );
   // Grab the following tx:
   // https://blockscout.karura.network/tx/0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e/token-transfers
   const tx = await evmprovider.getTXReceiptByHash(
@@ -17,9 +19,9 @@ async function main() {
   const recepit = await evmprovider.getTransactionByHash(
     "0xa82791bb02323ead8caa02adadd9fa2fde015d81bc170e5fd484306d060d016e"
   );
-  
+
   // tx & receipt are null
-  console.log(`tx: ${tx}\nreceipt: ${recepit}`);
+  console.log(`balance: ${balance.toString()}\ntx: ${tx}\nreceipt: ${recepit}`);
 }
 
 main();
